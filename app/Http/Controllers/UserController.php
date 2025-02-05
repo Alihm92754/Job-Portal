8c05210bb6751c293da9db9cab04ade5436d6535
Ali Hammoud <alihammoud92754@gmail.com>
2025-02-05 13:50:45 +0200

    design pages and forms

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,6 +41,7 @@ class UserController extends Controller
             'email' => request('email'),
             'password' => bcrypt(request('password')),
             'user_type' => self::JOB_POSTER,
+            'user_trial' => now()->addWeek(),
         ]);
 
         return redirect()->route('login')->with('successMessage', 'Your account was created successfully');
