8613c0f9bf2ee0f85e34d05ae32c47c493ce21a6
Ali Hammoud <alihammoud92754@gmail.com>
2025-02-05 02:04:22 +0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationFormRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    const JOB_SEEKER = 'seeker';
+    const JOB_POSTER = 'employer';
+
+    public function createSeeker()
+    {
+        return view('user.seeker-register');
+    }
+
+    public function createEmployer()
+    {
+        return view('user.employer-register');
+    }
+
+    public function storeSeeker(RegistrationFormRequest $request)
+    {
+        User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+            'user_type' => self::JOB_SEEKER,
+        ]);
+
+        return redirect()->route('login')->with('successMessage', 'Your account was created successfully');
+    }
+
+    public function storeEmployer(RegistrationFormRequest $request)
+    {
+        User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+            'user_type' => self::JOB_POSTER,
+        ]);
+
+        return redirect()->route('login')->with('successMessage', 'Your account was created successfully');
+    }
+
+    public function login()
+    {
+        return view('user.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        
+        if(Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+        return 'Wrong email or password';
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+}
+
